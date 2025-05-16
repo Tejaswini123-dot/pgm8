bb18e11f61308b15009d44bae0a1fc79afbcb4a2
teju <tejaswinir690@gmail.com>
2025-05-16 08:33:47 +0530

    first commit

---
+++ pgm8/src/main/java/com/Tejaswini123-dot/App.java
@@ -0,0 +1,13 @@
+package com.Tejaswini123-dot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
